1bf55accd93d23f4e504b0707e7583d0af083bf9
Artur <arturarsentiev25@gmail.com>
2024-11-11 16:23:30 +0200

    1451

--- src/com/softserve/edu15/hw/Main.java
+++ src/com/softserve/edu15/hw/Main.java
@@ -1,4 +1,19 @@
-package com.softserve.edu15.hw;
-
 public class Main {
+    public static void main(String[] args) {
+        int temperature = 32;
+        System.out.println(weatherInfo(temperature));
+    }
+
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c > 0)
+            return (c + " is freezing temperature");
+        else
+            return (c + " is above freezing temperature");
+    }
+
+    public static double convertToCelsius(int temperature) {
+        double celsius = (temperature - 32) * 5.0 / 9.0;
+        return celsius;
+    }
 }
